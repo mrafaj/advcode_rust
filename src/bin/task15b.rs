b0ce510119c881b149644e65fbb94d1ea1fe40ed
Matej Rafaj <matej.rafaj@fer.hr>
2023-12-01 13:26:02 +0100

    fixed tests for future tasks and added day 1 part 2 solution

--- src/bin/task15b.rs
+++ src/bin/task15b.rs
@@ -1,11 +1,12 @@
 use advcode_rust_template::file_reader;
 
 fn main() {
-    print!("{}", solver("data/dayNb.txt"));
+    print!("{}", solver("data/day01/test1.txt"));
 }
 
-fn solver(path : &str) -> T {
+fn solver(path : &str) -> u32 {
     let lines : Vec<String> = file_reader::lines_from_file(path);
+    0
 }
 
 #[cfg(test)]
@@ -14,7 +15,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = solver("data/dayNtest.txt");
-        assert_eq!(result, RESULT);
+        let result = solver("data/day01/test1.txt");
+        assert_eq!(result, 0);
     }
 }
